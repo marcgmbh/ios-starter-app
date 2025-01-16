f657cc710421bc8329e6fdef182e793c853f8fe4
marc <91703789+marcgbmh@users.noreply.github.com>
2025-01-16 17:04:47 +0100

    Initial Commit

---
+++ starter/starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  starterApp.swift
+//  starter
+//
+//  Created by marc on 16.01.25.
+//
+
+import SwiftUI
+
+@main
+struct starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ starterTests/starterTests.swift
@@ -0,0 +1,17 @@
+//
+//  starterTests.swift
+//  starterTests
+//
+//  Created by marc on 16.01.25.
+//
+
+import Testing
+@testable import starter
+
+struct starterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
