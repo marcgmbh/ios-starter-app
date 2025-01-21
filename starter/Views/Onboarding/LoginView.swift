af264e8ded8f8b51832610a508fd616a9c7c5149
marc <91703789+marcgbmh@users.noreply.github.com>
2025-01-21 01:32:06 +0100

    refactor: use app state manager

--- starter/Views/Onboarding/LoginView.swift
+++ starter/Views/Onboarding/LoginView.swift
@@ -1,15 +1,9 @@
-//
-//  LoginView.swift
-//  starter
-//
-//  Created by marc on 16.01.25.
-//
-
 import SwiftUI
 import Supabase
 
+@MainActor
 struct LoginView: View {
-    @EnvironmentObject private var appState: AppState
+    @StateObject private var appState = AppStateManager.shared
     @StateObject private var supabase = SupabaseManager.shared
     @State private var phoneNumber = ""
     @State private var verificationCode = ""
@@ -17,69 +11,22 @@ struct LoginView: View {
     @State private var showError = false
     @State private var errorMessage = ""
     @State private var showVerification = false
-    
     @FocusState private var isPhoneNumberFocused: Bool
     
     private var formattedPhoneNumber: String {
-        if !phoneNumber.starts(with: "+") {
-            return "1\(phoneNumber)"
-        }
-        return phoneNumber
+        phoneNumber.starts(with: "+") ? phoneNumber : "1\(phoneNumber)"
     }
     
     var body: some View {
         BottomButtonLayout {
-            VStack(spacing: 20) {
-                DescriptionText(text: showVerification ? "Enter Code" : "Enter Phone Number")
-                    .multilineTextAlignment(.center)
+            VStack {
+                DescriptionText(showVerification ? "Enter Code" : "Enter Phone Number")
                     .font(.system(size: 20, weight: .regular, design: .rounded))
-
+                
                 if !showVerification {
-                    VStack(spacing: 15) {
-                        InputFieldView(
-                            placeholder: "718-223-4425",
-                            text: $phoneNumber,
-                            keyboardType: .phonePad,
-                            textContentType: .telephoneNumber
-                        ) { newValue in
-                            phoneNumber = newValue.filter { $0.isNumber || $0 == "+" }
-                        }
-                        .padding()
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 30, weight: .regular, design: .rounded))
-                        .padding(.vertical, 10)
-                        .frame(minWidth: 100, maxWidth: .infinity)
-                        .cornerRadius(30)
-                        .focused($isPhoneNumberFocused)
-                    }
-                    .padding(.horizontal, 24)
+                    phoneNumberInput
                 } else {
-                    VStack(spacing: 15) {
-                        InputFieldView(
-                            placeholder: "461945",
-                            text: $verificationCode,
-                            keyboardType: .phonePad,
-                            textContentType: .telephoneNumber
-                        ) { newValue in
-                            verificationCode = newValue.filter { $0.isNumber }
-                        }
-                        .padding()
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 30, weight: .regular, design: .rounded))
-                        .padding(.vertical, 10)
-                        .frame(minWidth: 100, maxWidth: .infinity)
-                        .cornerRadius(30)
-                        .focused($isPhoneNumberFocused)
-                    }
-                    .padding(.horizontal, 24)
-                    
-                    Button(action: {
-                        showVerification = false
-                        verificationCode = ""
-                    }) {
-                        Text("Back")
-                            .foregroundColor(.blue)
-                    }
+                    verificationInput
                 }
                 
                 if showError {
@@ -90,39 +37,73 @@ struct LoginView: View {
             }
         } buttonContent: {
             if !showVerification {
-                Button(action: {
+                Button {
                     Task {
-                        if ["+16572145917", "16572145917", "6572145917"].contains(formattedPhoneNumber) {
-                            await loginWithEmail()
+                        if isTestNumber(formattedPhoneNumber) {
+//                            await loginWithEmail()
                         } else {
                             await sendVerificationCode()
                         }
                     }
-                }) {
-                    if isLoading {
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                    } else {
-                        PrimaryButton(myText: "Send Code")
-                    }
+                } label: {
+                    PrimaryButton(text: "Send Code", isLoading: isLoading)
                 }
                 .disabled(formattedPhoneNumber.count < 8 || isLoading)
             } else {
-                Button(action: {
+                Button {
                     Task {
                         await verifyCode()
                     }
-                }) {
-                    if isLoading {
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                    } else {
-                        PrimaryButton(myText: "Verify")
-                    }
+                } label: {
+                    PrimaryButton(text: "Verify", isLoading: isLoading)
                 }
-                .disabled(verificationCode.isEmpty)
+                .disabled(verificationCode.isEmpty || isLoading)
             }
         }
+    }
+    
+    private var phoneNumberInput: some View {
+        InputFieldView(
+            placeholder: "718-223-4425",
+            text: $phoneNumber,
+            keyboardType: .phonePad,
+            textContentType: .telephoneNumber
+        ) { newValue in
+            phoneNumber = newValue.filter { $0.isNumber || $0 == "+" }
+        }
+        .font(.system(size: 30, weight: .regular, design: .rounded))
+        .padding()
+        .multilineTextAlignment(.center)
+        .focused($isPhoneNumberFocused)
+    }
+    
+    private var verificationInput: some View {
+        VStack {
+            InputFieldView(
+                placeholder: "461945",
+                text: $verificationCode,
+                keyboardType: .phonePad,
+                textContentType: .oneTimeCode
+            ) { newValue in
+                verificationCode = newValue.filter { $0.isNumber }
+            }
+            .font(.system(size: 30, weight: .regular, design: .rounded))
+            .padding()
+            .multilineTextAlignment(.center)
+            .focused($isPhoneNumberFocused)
+            
+            Button {
+                showVerification = false
+                verificationCode = ""
+            } label: {
+                Text("Back")
+                    .foregroundColor(.blue)
+            }
+        }
+    }
+    
+    private func isTestNumber(_ number: String) -> Bool {
+        ["+16572145917", "16572145917", "6572145917"].contains(number)
     }
     
     private func sendVerificationCode() async {
@@ -146,43 +127,14 @@ struct LoginView: View {
         
         do {
             try await supabase.verifyOTP(phoneNumber: formattedPhoneNumber, code: verificationCode)
-            await MainActor.run {
-                appState.isLoggedIn = true
-                appState.moveToNextScreen()
-            }
+            appState.setLoggedIn(true)
+            appState.moveToNextScreen()
         } catch {
-            await MainActor.run {
-                showError = true
-                errorMessage = error.localizedDescription
-            }
+            showError = true
+            errorMessage = error.localizedDescription
         }
         
-        await MainActor.run {
-            isLoading = false
-        }
-    }
-    
-    private func loginWithEmail() async {
-        isLoading = true
-        do {
-            let result = try await supabase.client.auth.signIn(
-                email: "forapple@express.com",
-                password: "1234"
-            )
-            await MainActor.run {
-                supabase.session = result
-                appState.isLoggedIn = true
-                appState.moveToNextScreen()
-            }
-        } catch {
-            await MainActor.run {
-                showError = true
-                errorMessage = error.localizedDescription
-            }
-        }
-        await MainActor.run {
-            isLoading = false
-        }
+        isLoading = false
     }
 }
 
