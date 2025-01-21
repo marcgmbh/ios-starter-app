d48633c24817185e8eebb0e0a081c33fc905e10d
marc <91703789+marcgbmh@users.noreply.github.com>
2025-01-21 01:31:05 +0100

    chores: delete unused files

--- starter/PermissionsView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  PushNotificationsView.swift
-//  starter
-//
-//  Created by marc on 16.01.25.
-//
-
--- starter/Views/FriendsSectionView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  FriendSectionView.swift
-//  starter
-//
-//  Created by marc on 16.01.25.
-//
-
